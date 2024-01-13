e7ba3f38c1692ba984fb1f5d039b6dff68201df6
ahmedrem <156532821+ahmedrem@users.noreply.github.com>
2024-01-13 22:44:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,23 @@
 from flask import Flask
+import json
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, world!'
+def process():
+    response = '{"msg":"Server api is runing and waiting for requests..."}'
+    
+    return json.loads(response)
+
+
+@app.route('/process', methods=['POST'])
+def process():
+    response = ''
+    try:  
+        response = request.form["infos"]
+        
+    except:
+        response = '{"msg":"Failed to load data request !"}'
+
+    return json.loads(response)
