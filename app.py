73f923873df87315e1c1037275e4751442ccf643
ahmedrem <156532821+ahmedrem@users.noreply.github.com>
2024-01-13 22:56:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import json
 import os
 
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']
+
 
 @app.route('/')
 def wlcom():
