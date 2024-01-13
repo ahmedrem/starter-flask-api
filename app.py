70972cf0f9ce605e825b41834d32316f4b8e8994
ahmedrem <156532821+ahmedrem@users.noreply.github.com>
2024-01-13 22:48:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,16 +8,4 @@ app = Flask(__name__)
 def process():
     response = '{"msg":"Server api is runing and waiting for requests..."}'
     
-    return response
-
-
-@app.route('/process', method=['POST'])
-def process():
-    response = ''
-    try:  
-        response = request.form["infos"]
-        
-    except:
-        response = '{"msg":"Failed to load data request !"}'
-
     return json.loads(response)
