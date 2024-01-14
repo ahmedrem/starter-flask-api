df34b198398acaa2a39578850b4dafd49af99d67
ahmedrem <156532821+ahmedrem@users.noreply.github.com>
2024-01-14 20:54:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ import boto3
 import json
 import os
 
+#############################################################################
+
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.jpeg', '.png', '.gif', '.bmp']
-
 s3 = boto3.client('s3')
+
+#############################################################################
 
 def saveData(data):
     s3.put_object(
@@ -23,14 +26,14 @@ def loadData():
     )
     return json.loads(data['Body'].read())
 
+#############################################################################
 
 @app.route('/')
 def wlcom():
     response = '<marquee><h1>This is a flask API for the HPRAPP</h1><h2>Created by AhmedRem</h2></marquee>'
-    
     return response
 
-
+#############################################################################
 
 @app.route('/auth', methods=['POST'])
 def auth():
@@ -42,18 +45,15 @@ def auth():
     except Exception as e :
         response = str(e)
         #response = str(os.listdir(os.getcwd()))
-
     return response
 
-
+#############################################################################
 
 @app.route('/bigf', methods=['POST'])
 def bigf():
     response = ''
     try:  
-        response = request.get_json()
-        
+        response = request.get_json()   
     except:
         response = '{"msg":"Failed to load data request !"}'
-
     return response
